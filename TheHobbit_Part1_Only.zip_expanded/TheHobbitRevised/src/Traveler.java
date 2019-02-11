2a0d20205953f40b1400e8a85b87ab51e424ca31
Winnie Liang <19liangy49@stu.smuhsd.org>
2019-02-11 09:43:10 -0800

    2/11/19 9:43AM

---
+++ TheHobbit_Part1_Only.zip_expanded/TheHobbitRevised/src/Traveler.java
@@ -0,0 +1,28 @@
+public class Traveler {
+	
+	//fields
+	private int distanceTraveled;
+	private String name;
+	
+	
+	//constructor
+	public Traveler(String name) {
+		this.distanceTraveled = 0;
+		this.name = name;
+	}
+
+	
+	//methods
+	public int getDistanceTraveled() {
+		return distanceTraveled;
+	}
+	
+	public void travel(int miles) {
+		distanceTraveled += miles;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+}
